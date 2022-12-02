837db0b591d6ae6dd28591cde908735f330a8e20
Adithyan T <adithsreedhar628@gmail.com>
2022-12-02 20:59:51 +0530

    updated

--- script.js
+++ script.js
@@ -1,2 +1,66 @@
+class Calculator{
+    constructor(previousOperandTextElement,currentOperandTextElement){
+        this.currentOperandTextElement=currentOperandTextElement
+        this.previousOperandTextElement=previousOperandTextElement
+        this.clear()
+    }
+
+    clear(){
+        this.currentOperand=''
+        this.currentOperand=''
+        this.operation=undefined
+    }
+    delete(){
+
+    }
+    appendNumber(number){
+        if(number==='.'&& this.currentOperand.includes('.'))return
+        this.currentOperand=this.currentOperand.toString()  + number.toString()
+    }
+
+    chooseOperation(operation){
+        if(this.currentOperand=='')return
+        this.operation=operation
+        this.previousOperand = this.currentOperand
+        this.currentOperand=''
+    }
+
+    compute(){
+
+    }
+
+    updateDisplay(){
+        this.previousOperandTextElement.innerText = this.previousOperand
+        this.currentOperandTextElement.innerText = this.currentOperand
+    }
+
+
+}
+
+
+
+
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
+
+const calculator = new Calculator(previousOperandTextElement,currentOperandTextElement)
+
+numberButtons.forEach(button=>{
+    button.addEventListener('click',()=>{
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay();
+    })
+})
+ 
+operationButtons.forEach(button=>{
+    button.addEventListener('click',()=>{
+        calculator.chooseOperation(button.innerText)
+        calculator.updateDisplay();
+    })
+})
